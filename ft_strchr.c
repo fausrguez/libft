f27d32b93217b80ef016081b0522f02045b61a49
Faus Rodriguez <fausrguez@outlook.com>
2020-11-14 15:10:03 +0100

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,9 +6,10 @@
 /*   By: farodrig <farodrig@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 21:54:18 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/11/06 11:04:46 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/11/03 22:30:31 by farodrig      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 char	*ft_strchr(char *str, int c)
 {
--- ft_strchr.test.c
+++ ft_strchr.test.c
@@ -1,21 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_strchr.test.c                                   :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: farodrig <farodrig@student.codam.n>          +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2020/10/29 06:19:47 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/10/29 08:45:52 by farodrig      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <string.h>
-// #include <bsd/string.h>
-#define _GNU_SOURCE
 
-char	*ft_strchr(char *str, int c);
+char *ft_strchr(char *str, char c);
 
 void 				print_function_info(const char *name, const char *description)
 {
@@ -33,7 +19,7 @@ void 				print_success(const char *msg)
 	printf("\033[0;32m[SUCCESS]> %s\033[0m\n", msg);
 }
 
-void 				trigger_test(const char *test_description, char *str, int c)
+void 				trigger_test(const char *test_description, char *str, char c)
 {
 	char *ft_result;
 	char *lib_result;
@@ -48,23 +34,20 @@ void 				trigger_test(const char *test_description, char *str, int c)
 	{
 		print_error("lib_result is not the same as the ft_result");
 	}
-	printf("lib_result -> %s |%s|\n", lib_result, str);
-	printf("ft_result -> %s |%s|\n", ft_result, str);
+	printf("lib_result -> |%s|\n", lib_result);
+	printf("ft_result -> |%s|\n", ft_result);
 }
 
-int					main(void)
+int	main(void)
 {
 	char str[10];
-	int c;
+	char c;
 
-	print_function_info("strchr", "The strchr() function locates the first occurrence of c (converted to a char) in the string pointed to by s.  The terminating null character is considered to be part of the string; therefore if c is `\0', the func-tions locate the terminating `\0'.");
-	c = '\0';
-	strcpy(str, "\0");
-	trigger_test("Valid on both empty", str, c);
+	print_function_info("strchr", "The strchr() function returns a pointer to the first occur‐rence of the character c in the string s. return  a  pointer  to the  matched  character  or  NULL  if  the character is not found.");
 	strcpy(str, "hello");
+	c = '\0';
+	trigger_test("Valid on c = NULL", str, c);
 	c = 'l';
-	trigger_test("Valid on c = 'l'", str, c);
-	c = '9';
-	trigger_test("Valid on c = 9", str, c);
-	return (0);
+	trigger_test("Valid on c = l", str, c);
 }
+
