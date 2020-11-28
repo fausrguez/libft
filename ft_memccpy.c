db7e8ad598152d2452227e3e9993aa76a1a06265
Faus Rodriguez <farodrig@f0r4s2.codam.nl>
2020-11-28 14:47:53 +0100

    wpi

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,8 +6,24 @@
 /*   By: farodrig <farodrig@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/28 14:37:22 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/11/28 14:37:24 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/11/28 14:47:46 by farodrig      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+** 
+*/
+
+void *ft_memccpy(void *dest, const void *src, int c, t_lui n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	t_ui			i;
+
+	str1 = (unsigned char*)dest;
+	str2 = (unsigned char*)src;
+	
+	return (0);
+}
