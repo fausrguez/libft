846b820a329df3905134b5d59b89defcd4a7c002
Faus Rodriguez <farodrig@student.codam.nl>
2020-12-04 21:25:09 +0100

    libft.h updated

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: farodrig <farodrig@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/15 16:14:16 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/12/04 20:47:02 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/12/04 21:09:05 by farodrig      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,17 @@ int		ft_memcmp(const void *str1, const void *str2, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t len);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 void	*ft_memset(void *str, int c, size_t len);
-char	*ft_strcat(char *dest, char *src);
 char	*ft_strchr(char *str, int c);
 char	*ft_strdup(const char *str1);
+char	*ft_strjoin(char const *str1, char const *str2);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(char *str1, char *str2, size_t len);
 char	*ft_strnstr(const char *str1, const char *str2, size_t len);
 char	*ft_strrchr(char *str, int c);
+char	*ft_strtrim(char const *str1, char const *set);
+char	*ft_substr(char const *str, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
