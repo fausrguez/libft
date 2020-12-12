84c8c58304c9e4f2dd3a8283dd47e742e678be14
Faus Rodriguez <farodrig@student.codam.nl>
2020-12-12 16:58:06 +0100

    ft_putnbr_fd done

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,15 @@
 /*   By: farodrig <farodrig@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/12 16:40:03 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/12/12 16:52:20 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/12/12 16:57:56 by farodrig      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Outputs the integer 'n' to the given file descriptor
+*/
 
 void	ft_putnbr_fd(int n, int fd)
 {
