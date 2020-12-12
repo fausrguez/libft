1eb8c79deb535f809d00ba5d259c17ef869c1a10
Faus Rodriguez <farodrig@student.codam.nl>
2020-12-12 16:55:49 +0100

    ft_putendl_fd done

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,11 +6,15 @@
 /*   By: farodrig <farodrig@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/12 16:35:33 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/12/12 16:37:40 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/12/12 16:54:57 by farodrig      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Outputs the string 's' to the given filedescriptor, followed by a newline.
+*/
 
 void	ft_putendl_fd(char *str, int fd)
 {
