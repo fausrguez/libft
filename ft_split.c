d9d8bc45fc4561d96e6ff9ddcd6a39d1366e9a1f
Faus Rodriguez <farodrig@student.codam.nl>
2021-01-24 15:33:44 +0100

    ft_split done

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,55 @@
 /*   By: farodrig <farodrig@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/05 18:58:33 by farodrig      #+#    #+#                 */
-/*   Updated: 2021/01/10 15:09:15 by farodrig      ########   odam.nl         */
+/*   Updated: 2021/01/24 15:29:16 by farodrig      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static void	free_all(char **arr, unsigned int dimension)
+static int		cpy_word(
+	char **arr,
+	size_t dimension,
+	char *str,
+	size_t w_len)
+{
+	arr[dimension] = ft_calloc((w_len + 1), sizeof(char));
+	if (!arr[dimension])
+	{
+		return (0);
+	}
+	ft_strlcpy(arr[dimension], str, w_len + 1);
+	return (1);
+}
+
+static size_t	count_words(char *str, char c)
+{
+	char	*last_c;
+	size_t	words;
+
+	last_c = str - 1;
+	words = 0;
+	while (str)
+	{
+		if (*str == c || *str == 0)
+		{
+			if (str - last_c > 1)
+			{
+				words++;
+			}
+			last_c = str;
+			if (*str == 0)
+			{
+				break ;
+			}
+		}
+		str++;
+	}
+	return (words);
+}
+
+static void		free_all(char **arr, size_t dimension)
 {
 	while (dimension--)
 	{
@@ -22,58 +63,31 @@ static void	free_all(char **arr, unsigned int dimension)
 	free(arr);
 }
 
-static int	copy_buff(
-	char **arr,
-	unsigned int *dimension,
-	char *buff,
-	unsigned int buff_len
-)
+static void		str_split(char **arr, char *str, char c, size_t n_words)
 {
-	buff[buff_len++] = 0;
-	if (!(arr[*dimension] = (char *)ft_calloc(buff_len, sizeof(char))))
-	{
-		free(buff);
-		free_all(arr, *dimension);
-		return (0);
-	}
-	ft_strlcpy(arr[*dimension], buff, buff_len);
-	free(buff);
-	*dimension += 1;
-	return (1);
-}
+	char	*last_c;
+	size_t	dimension;
 
-static char	**str_split(
-	char *str,
-	char **arr,
-	char c,
-	size_t str_len
-)
-{
-	char			*buff;
-	unsigned int	buff_len;
-	unsigned int	dimension;
-
+	last_c = str - 1;
 	dimension = 0;
-	while (*str)
+	while (dimension < n_words)
 	{
-		if (!(buff = (char *)ft_calloc(str_len + 1, sizeof(char))))
+		if (*str == c)
 		{
-			free_all(arr, dimension);
-			return (0);
+			if (str - last_c > 1)
+			{
+				if (!cpy_word(arr, dimension, last_c + 1, (str - last_c) - 1))
+				{
+					free_all(arr, dimension);
+					return ;
+				}
+				dimension++;
+			}
+			last_c = str;
 		}
-		buff_len = 0;
-		while (*str && *str != c)
-		{
-			buff[buff_len++] = *str;
-			str++;
-		}
-		if (buff_len > 0)
-			if (copy_buff(arr, &dimension, buff, buff_len) == 0)
-				return (0);
 		str++;
 	}
 	arr[dimension] = 0;
-	return (arr);
 }
 
 /*
@@ -82,19 +96,17 @@ static char	**str_split(
 ** ended by a NULL pointer. Returns NULL if the allocation fails
 */
 
-char		**ft_split(char const *str, char c)
+char			**ft_split(char const *str, char c)
 {
 	char	**arr;
-	size_t	str_len;
+	size_t	n_words;
 
-	if (!str)
+	n_words = count_words((char *)str, c);
+	arr = ft_calloc(n_words + 1, sizeof(char *));
+	if (!arr)
 	{
 		return (0);
 	}
-	str_len = ft_strlen(str);
-	if (!(arr = (char **)ft_calloc(str_len + 1, sizeof(char *))))
-	{
-		return (0);
-	}
-	return (str_split((char *)str, arr, c, str_len));
+	str_split(arr, (char *)str, c, n_words);
+	return (arr);
 }
